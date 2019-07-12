0c05798dd23aabfd9e0d358086c28346426fd1db
le.loza <le.loza@PROYQALAB09.primeraplus.com>
2019-07-12 09:19:57 -0500

    La clase conexion incluye metodos para ejecutar todos los navegadores requeridos. EjecutarScripts agrega variables de parametros

--- PortalSTEP/src/test/java/bootpackage/claseConexion.java
+++ PortalSTEP/src/test/java/bootpackage/claseConexion.java
@@ -4,18 +4,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class claseConexion {
 
-	public String DRIVER_PATH_FIREFOX = "";
-	public String PROPERTY_PATH_FIREFOX = "webdriver.gecko.driver";
-	public String DRIVER_PATH_CHROME = "";
-	public String PROPERTY_PATH_CHROME = "webdriver.chrome.driver";
-	public String DRIVER_PATH_EDGE = "";
-	public String PROPERTY_PATH_EDGE = "webdriver.edge.driver";
+	public String DRIVER_PATH_FIREFOX;
+	public String PROPERTY_PATH_FIREFOX;
+	
+	public String DRIVER_PATH_CHROME;
+	public String PROPERTY_PATH_CHROME;
+	
+	public String DRIVER_PATH_EDGE;
+	public String PROPERTY_PATH_EDGE;
 	
 	public String URL_PORTAL = "http://qa-envios.primeraplus.com/";
 	public WebDriver driver;
+	
+///////////////////////////////////////////////////////////////////////////////////
+
+	public void iniciarVariables() {
+		
+		setDRIVER_PATH_CHROME("C:\\Users\\le.loza\\Downloads\\Drivers\\chromedriver.exe");
+		setPROPERTY_PATH_CHROME("webdriver.chrome.driver");
+
+		setDRIVER_PATH_FIREFOX("C:\\Users\\le.loza\\Downloads\\Drivers\\geckodriver.exe");
+		setPROPERTY_PATH_FIREFOX("webdriver.gecko.driver");
+		
+		setDRIVER_PATH_EDGE("C:\\Users\\le.loza\\Downloads\\Drivers\\Otra\\MicrosoftWebDriver.exe");
+		setPROPERTY_PATH_EDGE("webdriver.edge.driver");
+		
+	}
+	
+///////////////////////////////////////////////////////////////////////////////////
 	
 	public void ConexionChrome() {
 		
@@ -23,42 +43,112 @@ public class claseConexion {
 		String propertyPath;
 		
 		exePath = getDRIVER_PATH_CHROME();
-		propertyPath = "";
+		propertyPath = getPROPERTY_PATH_CHROME();
 		
+		System.setProperty(propertyPath, exePath);
+		driver = new ChromeDriver();
+		driver.get(getURL_PORTAL());
+	}
+	
+///////////////////////////////////////////////////////////////////////////////////
+
+	public void ConexionFirefox() {
 		
-		System.setProperty("webdriver.chrome.driver", exePath);
+		String exePath;
+		String propertyPath;
+		
+		exePath = getDRIVER_PATH_FIREFOX();
+		propertyPath = getPROPERTY_PATH_FIREFOX();
+		
+		System.setProperty(propertyPath, exePath);
+		driver = new FirefoxDriver();
+		driver.get(getURL_PORTAL());
+	}
+	
+///////////////////////////////////////////////////////////////////////////////////
+
+	public void ConexionEdge() {
+		
+		String exePath;
+		String propertyPath;
+		
+		exePath = getDRIVER_PATH_EDGE();
+		propertyPath = getPROPERTY_PATH_EDGE();
+		
+		System.setProperty(propertyPath, exePath);
+		driver = new EdgeDriver();
+		driver.get(getURL_PORTAL());
+	}
+
+	public void ConexionSafari() {
+		
 		driver = new SafariDriver();
 		driver.get(getURL_PORTAL());
 	}
 	
-	public WebDriver getDriver() {
-		return driver;
-	}
-	
-	public void setDriver(WebDriver driver) {
-		this.driver = driver;
-	}
+///////////////////////////////////////////////////////////////////////////////////
 	
 	public String getDRIVER_PATH_FIREFOX() {
 		return DRIVER_PATH_FIREFOX;
 	}
-	public void setDRIVER_PATH_FIREFOX(String driver_path_firefox) {
-		DRIVER_PATH_FIREFOX = driver_path_firefox;
+
+	public void setDRIVER_PATH_FIREFOX(String DRIVER_PATH_FIREFOX) {
+		this.DRIVER_PATH_FIREFOX = DRIVER_PATH_FIREFOX;
 	}
+	
+	public String getPROPERTY_PATH_FIREFOX() {
+		return PROPERTY_PATH_FIREFOX;
+	}
+
+	public void setPROPERTY_PATH_FIREFOX(String PROPERTY_PATH_FIREFOX) {
+		this.PROPERTY_PATH_FIREFOX = PROPERTY_PATH_FIREFOX;
+	}
+	
+///////////////////////////////////////////////////////////////////////////////////
 	
 	public String getDRIVER_PATH_CHROME() {
 		return DRIVER_PATH_CHROME;
 	}
-	
-	public void setDRIVER_PATH_CHROME(String driver_path_chrome) {
-		DRIVER_PATH_CHROME = driver_path_chrome;
+
+	public void setDRIVER_PATH_CHROME(String DRIVER_PATH_CHROME) {
+		this.DRIVER_PATH_CHROME = DRIVER_PATH_CHROME;
+	}
+
+	public String getPROPERTY_PATH_CHROME() {
+		return PROPERTY_PATH_CHROME;
+	}
+
+	public void setPROPERTY_PATH_CHROME(String PROPERTY_PATH_CHROME) {
+		this.PROPERTY_PATH_CHROME = PROPERTY_PATH_CHROME;
 	}
 	
+///////////////////////////////////////////////////////////////////////////////////
+
+	public String getDRIVER_PATH_EDGE() {
+		return DRIVER_PATH_EDGE;
+	}
+
+	public void setDRIVER_PATH_EDGE(String DRIVER_PATH_EDGE) {
+		this.DRIVER_PATH_EDGE = DRIVER_PATH_EDGE;
+	}
+
+	public String getPROPERTY_PATH_EDGE() {
+		return PROPERTY_PATH_EDGE;
+	}
+
+	public void setPROPERTY_PATH_EDGE(String PROPERTY_PATH_EDGE) {
+		this.PROPERTY_PATH_EDGE = PROPERTY_PATH_EDGE;
+	}
+	
+///////////////////////////////////////////////////////////////////////////////////
+
 	public String getURL_PORTAL() {
 		return URL_PORTAL;
 	}
-	
-	public void setURL_PORTAL(String url_portal) {
-		URL_PORTAL = url_portal;
+
+	public void setURL_PORTAL(String uRL_PORTAL) {
+		URL_PORTAL = uRL_PORTAL;
 	}
+	
+	
 }
---
+++ PortalSTEP/src/test/java/portalSTEP/TestPortal3Guias.java
@@ -0,0 +1,45 @@
+package portalSTEP;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import bootpackage.claseConexion;
+
+public class TestPortal3Guias extends claseConexion {
+
+	claseConexion conexion = new claseConexion();
+
+	@Parameters({"browser"})
+	@BeforeClass
+	public void test3Conexion(String browser) throws InterruptedException {
+
+		iniciarVariables();
+		System.out.println(browser);
+		if(browser.equalsIgnoreCase("firefox")) {
+
+			ConexionFirefox();
+		}		
+		else if(browser.equalsIgnoreCase("edge")){
+
+			ConexionEdge();
+		}
+		else if(browser.equalsIgnoreCase("chrome")){
+
+			ConexionChrome();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+		
+		driver.quit();
+	}
+	
+	@Test(dataProvider = "browsers")
+	public void saludo() {
+		System.out.println("hola");
+	}
+}
